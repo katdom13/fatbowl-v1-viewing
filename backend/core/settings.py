2ddaad3004b23b2cac49a171471b35e137875f5b
katdom13 <katdom13@gmail.com>
2021-08-25 16:31:41 +0800

    Installed django-environ for environment variables, implemented containerization using Dockerfile and docker-compose.yml

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,20 +14,30 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True)
+)
+# reading .env file
+environ.Env.read_env()
+
+FROM_DOCKER = os.environ.get('FROM_DOCKER', False)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_%h)8=!*hrymm5m04tnn_*8qt&s(yla*!w@roh0e+l0z!lx0&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -207,12 +217,12 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL_HOST_USER = env("EMAIL_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASS")
 
 # PAYPAL
-FATOWL_CLIENT_ID = os.environ.get('FATOWL_CLIENT_ID')
-FATOWL_CLIENT_SECRET = os.environ.get('FATOWL_CLIENT_SECRET')
+FATOWL_CLIENT_ID = env('FATOWL_CLIENT_ID')
+FATOWL_CLIENT_SECRET = env('FATOWL_CLIENT_SECRET')
 
 # PASSWORD CHANGE
 LOGOUT_ON_PASSWORD_CHANGE = False
